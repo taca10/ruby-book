a01554dde7685982ba5a8d562077f6da1ebe2340
taca10 <taka.ur.0505@gmail.com>
2018-03-17 17:19:44 +0900

    ミニテスト作成

---
+++ test/fizz_buzz.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require './lib/fizz_buzz'
+# 自分のファイルパスを起点として、読み込むファイルを指定します require_relative '../lib/fizz_buzz'
+
+class FizzBuzzTest < Minitest::Test
+  def test_fizz_buzz
+    assert_equal '1', fizz_buzz(1)
+    assert_equal '2', fizz_buzz(2)
+    assert_equal 'Fizz', fizz_buzz(3)
+    assert_equal '4', fizz_buzz(4)
+    assert_equal 'Buzz', fizz_buzz(5)
+    assert_equal 'Fizz', fizz_buzz(6)
+    assert_equal 'Fizz Buzz', fizz_buzz(15)
+  end
+end
