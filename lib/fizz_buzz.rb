c34daa9ed20d62c3d9b10ca36c0f107ba54d353f
taca10 <taka.ur.0505@gmail.com>
2018-03-17 17:18:32 +0900

    コメントアウト

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -10,6 +10,10 @@ def fizz_buzz(n)
   end
 end
 
-puts fizz_buzz(3)
-puts fizz_buzz(5)
-puts fizz_buzz(15)
+# puts fizz_buzz(1)
+# puts fizz_buzz(2)
+# puts fizz_buzz(3)
+# puts fizz_buzz(4)
+# puts fizz_buzz(5)
+# puts fizz_buzz(6)
+# puts fizz_buzz(15)
